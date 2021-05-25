5f8f37bbce086a6c3611bb42b139300faa3cf783
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-24 17:58:45 -0700

    created database set up based on assignment Readme file.

---
+++ assets/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS schema_DB;
+
+CREATE DATABASE schema_DB;
+
+USE schema_DB;
+
+CREATE TABLE department (
+id  INT PRIMARY KEY,
+name  VARCHAR(30) --to hold department name
+);
+
+
+CREATE TABLE role (
+id INT PRIMARY KEY,
+title VARCHAR(30),      --to hold role title
+salary DECIMAL,         --to hold role salary
+department_id INT       --to hold reference to department role belongs to
+);
+
+
+
+CREATE TABLE employee(
+id - INT PRIMARY KEY,
+first_name - VARCHAR(30),   --to hold employee first name
+last_name - VARCHAR(30),    --to hold employee last name
+role_id - INT,              --to hold reference to role employee has
+manager_id - INT            --to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+);
+
