3f938579b878ba16562fab37f349874d79a99f1f
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-25 22:25:10 -0700

    Uncommented out queries to join tables

--- assets/csmSeeds.sql
+++ assets/csmSeeds.sql
@@ -71,15 +71,16 @@ SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
 
-SELECT employee.id AS ID, employee.first_name AS First_name, employee.last_name AS Last_name, role.title AS Title, department.name AS Department, role.salary AS Salary, employee.manager_id AS Manager
-FROM employee e
-JOIN role ON employee.role_id = role.id 
-JOIN department ON role.department_id = department.id
-LEFT JOIN employee e ON employee.id = e.manager_id; -- get manager ID to instead show Manager name
+-- 1st method
+-- SELECT employee.id AS ID, employee.first_name AS First_name, employee.last_name AS Last_name, role.title AS Title, department.name AS Department, role.salary AS Salary, employee.manager_id AS Manager
+-- FROM employee 
+-- JOIN role ON employee.role_id = role.id 
+-- JOIN department ON role.department_id = department.id
+-- LEFT JOIN employee ON employee.id = employee.manager_id; 
 
 -- 2nd method
--- SELECT DISTINCT e1.id AS ID, e1.first_name AS First_name, e1.last_name AS Last_name, r.title AS Title, department.name AS Department, r.salary AS Salary, e1.manager_id AS Manager
--- FROM employee e1, employee e2
--- LEFT JOIN role r ON employee.role_id = r.id 
--- LEFT JOIN department ON r.department_id = department.id
--- LEFT JOIN employee e ON e1.id = e2.manager_id;
+SELECT DISTINCT e1.id AS ID, e1.first_name AS First_name, e1.last_name AS Last_name, r.title AS Title, department.name AS Department, r.salary AS Salary, e1.manager_id AS Manager
+FROM employee e1, employee e2
+LEFT JOIN role r ON employee.role_id = r.id 
+LEFT JOIN department ON r.department_id = department.id
+LEFT JOIN employee e ON e1.id = e2.manager_id;
