308309d2e0d2cbeeae2d9d51c12752e59b662749
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-26 17:50:54 -0700

    used CONCAT to combine manager first and last name in manager column

--- assets/queries.sql
+++ assets/queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
 
-SELECT employee.id AS ID, employee.first_name AS First_name, employee.last_name AS Last_name, role.title AS Title, department.name AS Department, role.salary AS Salary, manager.Last_name AS Manager
+SELECT employee.id AS ID, employee.first_name AS First_name, employee.last_name AS Last_name, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT (manager.Last_name, ', ', manager.First_name) AS Manager
 FROM employee 
 LEFT JOIN role ON employee.role_id = role.id 
 LEFT JOIN department ON role.department_id = department.id
