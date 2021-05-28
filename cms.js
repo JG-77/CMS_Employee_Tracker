58d113deee93668c8d190c97496d0bc3d18d1581
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-27 21:52:22 -0700

    Fixed the syntax error on viewEmployeeByDept() but data does not display correctly

--- cms.js
+++ cms.js
@@ -84,8 +84,8 @@ function viewEmployeeByDept() {
               FROM employee 
               LEFT JOIN role ON employee.role_id = role.id 
               LEFT JOIN department ON role.department_id = department.id
-              LEFT JOIN employee manager ON manager.id = employee.manager_id;
-              WHERE ?`, 
+              LEFT JOIN employee manager ON manager.id = employee.manager_id
+              WHERE department.name !=?;`, 
               [
                 {
                 name: response.dept,
